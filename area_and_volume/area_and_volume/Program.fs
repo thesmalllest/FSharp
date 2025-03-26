904becff244dda34d0668fed3be3ce3ca2c163be
thesmalllest <vikulik.vs.137@gmail.com>
2025-03-27 02:53:57 +0300

    Task 5.4
    Написана рекурсивная функция для нахождения суммы цифр числа. Использована рекурсия вверх.

--- area_and_volume/area_and_volume/Program.fs
+++ area_and_volume/area_and_volume/Program.fs
@@ -8,23 +8,36 @@ let cylinderVolume radius height : float =
     let area = circleArea radius
     height * area
 
+let rec sumDigits number : int = 
+    if number = 0 then 0
+    else 
+        (number % 10) + sumDigits(number / 10)
+
 [<EntryPoint>]
 let main argv =
-    Console.WriteLine("Введите радиус:")
+    //Объем цилиндра
+    Console.WriteLine("Введите радиус цилиндра:")
     let radius = Console.ReadLine() |> float
-
-    Console.WriteLine("Введите высоту:")
+    Console.WriteLine("Введите высоту цилиндра:")
     let height = Console.ReadLine() |> float
 
-    //Суперпозиция
+        //Суперпозиция
     let countVolume (r, h) = cylinderVolume r h
     let printVolume v = Console.WriteLine($"Объём цилиндра: {v}")
     let volume = countVolume >> printVolume
     volume (radius, height)
 
-    //Каррирование
+        //Каррирование
     let volumeFunction = cylinderVolume radius
     let result = volumeFunction height
     Console.WriteLine($"Объём цилиндра: {result}")
+
+    //Сумма цифр
+    Console.WriteLine("Введите число, чтобы посчитать сумму его цифр:")
+    let input = Console.ReadLine()
+    let number = int input  
+
+    let sum = sumDigits number
+    Console.WriteLine($"Сумма цифр {number} равна {sum}")
 
     0
