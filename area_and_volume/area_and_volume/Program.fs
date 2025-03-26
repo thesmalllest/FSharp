fe1002e1cb25174d6b9d0d7c0af76edf69f33b30
thesmalllest <vikulik.vs.137@gmail.com>
2025-03-27 00:39:00 +0300

    Task 5.3
    Написана функция вычисления площади круга и другая
    функция вычисления объема цилиндра с использованием написанной функции.

---
+++ area_and_volume/area_and_volume/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+let circleArea radius : float =
+    let pi = 3.14159
+    pi * radius * radius
+
+let cylinderVolume radius height : float =
+    let area = circleArea radius
+    height * area
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите радиус:")
+    let radius = Console.ReadLine() |> float
+
+    Console.WriteLine("Введите высоту:")
+    let height = Console.ReadLine() |> float
+
+    //Суперпозиция
+    let countVolume (r, h) = cylinderVolume r h
+    let printVolume v = Console.WriteLine($"Объём цилиндра: {v}")
+    let volume = countVolume >> printVolume
+    volume (radius, height)
+
+    //Каррирование
+    let volumeFunction = cylinderVolume radius
+    let result = volumeFunction height
+    Console.WriteLine($"Объём цилиндра: {result}")
+
+    0
