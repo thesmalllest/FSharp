eb79230a9ac5eb94c70b5489425a27744327eadf
thesmalllest <vikulik.vs.137@gmail.com>
2025-03-27 10:52:30 +0300

    Task 5.5
    Написана рекурсивная функция с телом выражение для нахождения суммы цифр числа. Использована рекурсия вниз.

--- area_and_volume/area_and_volume/Program.fs
+++ area_and_volume/area_and_volume/Program.fs
@@ -8,14 +8,26 @@ let cylinderVolume radius height : float =
     let area = circleArea radius
     height * area
 
-let rec sumDigits number : int = 
+//Рекурсия вверх
+let rec sumDigitsUp number : int = 
     if number = 0 then 0
     else 
-        (number % 10) + sumDigits(number / 10)
+        (number % 10) + sumDigitsUp(number / 10)
+
+//Рекурсия вниз
+let sumDigitsDown n = 
+    let rec sum n curSum = 
+        if n = 0 then curSum
+        else
+            let n1 = n / 10
+            let digit = n % 10
+            let result = curSum + digit
+            sum n1 result 
+    sum n 0
 
 [<EntryPoint>]
 let main argv =
-    //Объем цилиндра
+(*    //Объем цилиндра
     Console.WriteLine("Введите радиус цилиндра:")
     let radius = Console.ReadLine() |> float
     Console.WriteLine("Введите высоту цилиндра:")
@@ -30,14 +42,17 @@ let main argv =
         //Каррирование
     let volumeFunction = cylinderVolume radius
     let result = volumeFunction height
-    Console.WriteLine($"Объём цилиндра: {result}")
+    Console.WriteLine($"Объём цилиндра: {result}")*)
 
     //Сумма цифр
     Console.WriteLine("Введите число, чтобы посчитать сумму его цифр:")
     let input = Console.ReadLine()
     let number = int input  
 
-    let sum = sumDigits number
-    Console.WriteLine($"Сумма цифр {number} равна {sum}")
+    let sum_1 = sumDigitsUp number
+    Console.WriteLine($"Сумма цифр {number} равна {sum_1}")
+
+    let sum_2 = sumDigitsDown number
+    Console.WriteLine($"Сумма цифр {number} равна {sum_2}")
 
     0
