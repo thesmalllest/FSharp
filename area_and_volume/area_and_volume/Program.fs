9db02c34bef45fc60d06d1abefeff6279d1abda2
thesmalllest <vikulik.vs.137@gmail.com>
2025-04-03 14:27:29 +0300

    Count Devisiors using Recursion

--- area_and_volume/area_and_volume/Program.fs
+++ area_and_volume/area_and_volume/Program.fs
@@ -25,9 +25,27 @@ let sumDigitsDown n =
             sum n1 result 
     sum n 0
 
+//Количество делителей Вверх
+let rec countDevisiorsUp x index : int =
+    match index with
+    | index when (x % index = 0 && index < x) -> 1 + countDevisiorsUp x (index + 1)
+    | index when (index >= x) -> 0
+    | _ -> 0 + countDevisiorsUp x (index + 1)
+
+//Количество делителей Вниз
+let countDevisiorsDown x = 
+    let rec countDevisiorsDown x index sum =
+        let isNeed = (x % index = 0) && (index < x)
+        let new_sum = sum + 1
+        match isNeed with
+            | true -> countDevisiorsDown x (index + 1) (new_sum)
+            | false when index < x -> countDevisiorsDown x (index + 1) sum
+            | _ -> sum
+    countDevisiorsDown x 1 0
+
 [<EntryPoint>]
 let main argv =
-(*    //Объем цилиндра
+    //Объем цилиндра
     Console.WriteLine("Введите радиус цилиндра:")
     let radius = Console.ReadLine() |> float
     Console.WriteLine("Введите высоту цилиндра:")
@@ -42,17 +60,35 @@ let main argv =
         //Каррирование
     let volumeFunction = cylinderVolume radius
     let result = volumeFunction height
-    Console.WriteLine($"Объём цилиндра: {result}")*)
+    Console.WriteLine($"Объём цилиндра: {result}")
 
     //Сумма цифр
     Console.WriteLine("Введите число, чтобы посчитать сумму его цифр:")
-    let input = Console.ReadLine()
-    let number = int input  
+    let input_for_sum = Console.ReadLine()
+    let number_for_sum = int input_for_sum  
 
-    let sum_1 = sumDigitsUp number
-    Console.WriteLine($"Сумма цифр {number} равна {sum_1}")
+    let sum_1 = sumDigitsUp number_for_sum
+    Console.WriteLine($"Сумма цифр {number_for_sum} равна {sum_1}")
 
-    let sum_2 = sumDigitsDown number
-    Console.WriteLine($"Сумма цифр {number} равна {sum_2}")
+    let sum_2 = sumDigitsDown number_for_sum
+    Console.WriteLine($"Сумма цифр {number_for_sum} равна {sum_2}")
+
+
+    Console.WriteLine("Введите число, чтобы посчитать количество делителей:")
+    let input_for_count = Console.ReadLine()
+    let number_for_count = int input_for_count
+
+    let count_1 = countDevisiorsUp number_for_count 1
+    Console.WriteLine($"Количество делителей {number_for_count}: {count_1}")
+
+    let count_2 = countDevisiorsDown number_for_count
+    Console.WriteLine($"Количество делителей {number_for_count}: {count_2}")
 
     0
+
+
+
+
+
+
+
