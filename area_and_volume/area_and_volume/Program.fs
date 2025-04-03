1062825da977272488f91678db65ab6ece415275
thesmalllest <vikulik.vs.137@gmail.com>
2025-04-03 14:48:41 +0300

    Task 5.6

--- area_and_volume/area_and_volume/Program.fs
+++ area_and_volume/area_and_volume/Program.fs
@@ -1,5 +1,6 @@
 ﻿open System
 
+// task 3
 let circleArea radius : float =
     let pi = 3.14159
     pi * radius * radius
@@ -8,13 +9,13 @@ let cylinderVolume radius height : float =
     let area = circleArea radius
     height * area
 
-//Рекурсия вверх
+// task 4
 let rec sumDigitsUp number : int = 
     if number = 0 then 0
     else 
         (number % 10) + sumDigitsUp(number / 10)
 
-//Рекурсия вниз
+// task 5
 let sumDigitsDown n = 
     let rec sum n curSum = 
         if n = 0 then curSum
@@ -25,14 +26,14 @@ let sumDigitsDown n =
             sum n1 result 
     sum n 0
 
-//Количество делителей Вверх
+// Количество делителей Вверх
 let rec countDevisiorsUp x index : int =
     match index with
     | index when (x % index = 0 && index < x) -> 1 + countDevisiorsUp x (index + 1)
     | index when (index >= x) -> 0
     | _ -> 0 + countDevisiorsUp x (index + 1)
 
-//Количество делителей Вниз
+// Количество делителей Вниз
 let countDevisiorsDown x = 
     let rec countDevisiorsDown x index sum =
         let isNeed = (x % index = 0) && (index < x)
@@ -43,9 +44,24 @@ let countDevisiorsDown x =
             | _ -> sum
     countDevisiorsDown x 1 0
 
+// task 6
+let factorial n =
+    let rec mul n acc =
+        match n with
+            | 0 | 1 -> acc
+            | _ -> mul(n-1) (acc*n)
+    mul n 1
+
+let isTrue (b:bool) =
+    match b with
+        | true -> factorial 5
+        | false -> sumDigitsDown 123
+
+
+
 [<EntryPoint>]
 let main argv =
-    //Объем цилиндра
+(*    //Объем цилиндра
     Console.WriteLine("Введите радиус цилиндра:")
     let radius = Console.ReadLine() |> float
     Console.WriteLine("Введите высоту цилиндра:")
@@ -83,12 +99,8 @@ let main argv =
 
     let count_2 = countDevisiorsDown number_for_count
     Console.WriteLine($"Количество делителей {number_for_count}: {count_2}")
+*)  
+    // 6
+    Console.WriteLine(isTrue false)
 
     0
-
-
-
-
-
-
-
