0862c3a6d9a862bface745d04f62ae7a07831da8
thesmalllest <vikulik.vs.137@gmail.com>
2025-04-10 14:45:39 +0300

    lab 6 task 1.17

--- working_with_lists/working_with_lists/Program.fs
+++ working_with_lists/working_with_lists/Program.fs
@@ -7,11 +7,27 @@ let shiftRight list =
         let last = List.last list
         last::(list |> List.take (List.length list - 1))
 
+let swapMinMax list =
+    let minEl = List.min list
+    let maxEl = List.max list
+    let rec swap l = 
+        match l with
+        | [] -> []
+        | h::t when h = minEl -> maxEl :: (swap t)
+        | h::t when h = maxEl -> minEl :: (swap t)
+        | h::t -> h :: (swap t)
+    swap list
+
+
+
 [<EntryPoint>]
 let main argv = 
     let l = [1; 2; 3; 4; 5]
 
     let result_1_7 = shiftRight (shiftRight l)
     printfn "%A" result_1_7
+
+    let result_1_17 = swapMinMax l
+    printfn "%A" result_1_17
 
     0
