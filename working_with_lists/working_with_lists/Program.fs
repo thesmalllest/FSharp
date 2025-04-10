262d994d2bb8602ad4dcce528ccb022eb7e85326
thesmalllest <vikulik.vs.137@gmail.com>
2025-04-10 14:30:03 +0300

    lab 6 task 1.7

---
+++ working_with_lists/working_with_lists/Program.fs
@@ -0,0 +1,17 @@
+﻿// task 1.7
+
+let shiftRight list =
+    match list with
+    | [] -> []
+    | _ -> 
+        let last = List.last list
+        last::(list |> List.take (List.length list - 1))
+
+[<EntryPoint>]
+let main argv = 
+    let l = [1; 2; 3; 4; 5]
+
+    let result_1_7 = shiftRight (shiftRight l)
+    printfn "%A" result_1_7
+
+    0
