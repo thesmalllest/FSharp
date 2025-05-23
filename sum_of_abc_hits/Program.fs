f0a225e064be50c72e2649a3a5579da4581e40bc
thesmalllest <vikulik.vs.137@gmail.com>
2025-05-23 19:36:33 +0300

    gcd method
    Wrote first method gcd using tail recursion for my individual task and tested it.

---
+++ sum_of_abc_hits/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let rec gcd a b =
+    match b with
+    | 0 -> a
+    | _ -> gcd b (a % b)
+
+[<EntryPoint>]
+let main argv = 
+
+    Console.WriteLine(gcd 5 27)
+    Console.WriteLine(gcd 3 27)
+
+    0
